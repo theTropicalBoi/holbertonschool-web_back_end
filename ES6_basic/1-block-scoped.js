3ad20e769412cfe679997c8016a3d6b94c9471d6
Shaan Somarchand <153312506+theTropicalBoi@users.noreply.github.com>
2025-03-29 22:47:13 +0100

    Exercise

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const innerTask = true;
+    const innerTask2 = false;
+    console.log(innerTask, innerTask2);
+  }
+
+  return [task, task2];
+}
