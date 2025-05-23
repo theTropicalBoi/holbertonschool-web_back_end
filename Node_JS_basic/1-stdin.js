cd29d7d713ee060cd48724193224000038a42e51
Shaan Somarchand <153312506+theTropicalBoi@users.noreply.github.com>
2025-05-23 11:32:27 +0200

    Task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  console.log(`Your name is: ${name}`);
+  process.exit();
+});
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
